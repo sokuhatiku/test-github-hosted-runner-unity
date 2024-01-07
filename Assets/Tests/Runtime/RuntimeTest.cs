a2b0e1a985de661fe71c275b77d9b2917308ab29
Sokuhatiku <sokuhatiku@gmail.com>
2024-01-07 21:56:30 +0900

    Add Unity project

---
+++ Assets/Tests/Runtime/RuntimeTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.Runtime
+{
+    public class RuntimeTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
